5ad2406d7d366bbae5c7961aa2092054ddc7625a
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-03-25 19:22:10 -0400

    Rewrite auto-js.js to use the new class-based addons system

--- mods/auto-js.js
+++ mods/auto-js.js
@@ -1,17 +1,20 @@
-return {
-    id: "auto-js",
-    name: "Auto JS",
-    description: "Automatically, by default, enable JavaScript extensions.",
-    version: "1.0.0",
-    author: "codingisfun2831",
-    depends: [],
-    doMenu: false,
+return class extends Mod {
+    // Metadata
+    ID = "auto-js"; // the id of the addon
+    NAME = "Auto JS"; // human-readable name
+    DESCRIPTION = "Automatically, by default, enable JavaScript extensions."; // description
+    VERSION = "1.1.0"; // version
+    AUTHOR = "codingisfun2831"; // author
+    DEPENDS = []; // dependencies (addon ids, useful for libraries)
+    DO_MENU = false; // whether to add a menu item
 
+    // Main function - gets ran when the addon is loaded
     main() {
         this.startingValue = Process.prototype.enableJS;
         Process.prototype.enableJS = true;
-    },
+    }
 
+    // Cleanup function - get ran when the addon is "deleted"
     cleanupFunc() {
         Process.prototype.enableJS = this.startingValue;
     }
