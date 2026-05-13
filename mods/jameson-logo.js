6f82bc20c7a0a4af252e56d6a99e5c96d654d21d
SparkleBot <mail@example.com>
2026-05-13 00:04:06 +0000

    Format JavaScript code

--- mods/jameson-logo.js
+++ mods/jameson-logo.js
@@ -28,7 +28,7 @@ return class extends Mod {
     AUTHOR = "PPPDUD";
     DEPENDS = [];
     DO_MENU = false;
-    
+
     main() {
         this.api.disallowSnaps("Jameson");
         this.api.ide.createLogo = function() {
@@ -41,7 +41,7 @@ return class extends Mod {
             this.logo.mouseClickLeft = old.mouseClickLeft;
             this.add(this.logo);
         }
-        this.api.ide.refreshIDE(); 
+        this.api.ide.refreshIDE();
     }
 
     cleanupFunc() {
