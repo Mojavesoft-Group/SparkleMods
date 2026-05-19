6f66135d21c3290518ad213ef7906a19300a5965
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2026-05-19 19:38:36 -0400

    Change version number back

--- mods/jameson-logo.js
+++ mods/jameson-logo.js
@@ -24,7 +24,7 @@ return class extends Mod {
     ID = "jameson-logo";
     NAME = "Jameson Logo";
     DESCRIPTION = "Replace the Snap! logo with Jameson's logo.";
-    VERSION = "1.1.0a";
+    VERSION = "1.1.0";
     AUTHOR = "PPPDUD";
     DEPENDS = [];
     DO_MENU = false;
